a33b1bb0e87c2a5118124251825e748e4990b521
DaniilT048 <tis.daniil98@gmail.com>
2025-04-27 15:17:09 +0300

    maked interface and state

--- src/store/toDoSlice.ts
+++ src/store/toDoSlice.ts
@@ -1,31 +1,15 @@
 import {createSlice, Slice} from '@reduxjs/toolkit';
 import {useState} from "react";
 
-interface ToDoSlice{
-    newTask: string;
+interface Task{
     text: string;
     done: boolean;
 }
 
-const initialState: ToDoSlice ={
-    newTask: '',
-    text: 'new task',
-    done: false,
+interface ToDoState{
+    tasks: Task[];
 }
 
-const toDoSlice:Slice<ToDoSlice> = createSlice({
-    name: 'toDoSlice',
-    initialState,
-    reducers:{
-        addTask(state: ToDoSlice){
-            if (state.newTask === '') {
-                return
-            }
-            state.setTasks([...state.tasks, {text: newTask, done: false}])
-        },
-        handleNewTask(state: ToDoSlice):void {
-            setNewTask(state.target.value)
-        }
-        },
-
-    });
+const initialState: ToDoState = {
+    tasks: [],
+}
